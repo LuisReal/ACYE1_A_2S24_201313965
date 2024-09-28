ea5ab2efdabbc96bc139b24e5dca042506f541ca
LuisReal <lgonzalezr2611@gmail.com>
2024-09-27 20:38:13 -0600

    menu y submenu
    menu principal y submenu de forma de ingreso manul o carca archivo csv

---
+++ Practica4/ordenamiento.s
@@ -0,0 +1,134 @@
+.global _start
+
+.data
+    
+    //clear es para limpiar la terminal
+    clear:
+        .asciz "\x1B[2J\x1B[H"
+        lenClear = . - clear
+
+    encabezado:
+        .asciz "Universidad de San Carlos de Guatemala\n"
+        .asciz "Facultad de Ingenieria\n"
+        .asciz "Escuela de Ciencias y Sistemas\n"
+        .asciz "Arquitectura de Computadores y Ensambladores 1\n"
+        .asciz "Seccion A\n"
+        .asciz "Luis Fernando Gonzalez Real\n"
+        .asciz "201313965\n"
+        lenEncabezado = . - encabezado
+
+    menuPrincipal:
+        .asciz ">> Menu Principal\n"
+        .asciz "1. Ingreso de lista de numeros\n"
+        .asciz "2. Bubble Sort\n"
+        .asciz "3. Quick Sort\n"
+        .asciz "4. Insertion Sort\n"
+        .asciz "5. Merge Sort\n"
+        .asciz "6. Salir\n"
+        lenMenu = .- menuPrincipal
+
+    subMenu:
+        .asciz ">> subMenu\n"
+        .asciz "1. De forma manual\n"
+        .asciz "2. Carga de Archivo csv\n"
+        .asciz "3. Regresar al menu anterior\n"
+        lensubMenu = .- subMenu
+
+    Opcion:
+        .asciz "Ingrese Una Opcion: "
+        lenOpcion = .- Opcion
+
+    continuar:
+        .asciz "Debe presionar la tecla enter para continuar\n"
+        lenContinuar = . - continuar
+    
+    errormsg:
+        .asciz "ERROR\n"
+        lenErrormsg = . - errormsg
+
+    operacionComa:
+        .asciz "Ingrese los numeros separados por coma: "
+        lenOperacionComa = . - operacionComa
+
+.bss
+    opcion:
+        .space 5
+    
+    operacion:
+        .space 20   // => Reserva espacion en bytes para operacion
+
+//metodo print para imprimir en consola
+.macro print texto, cantidad
+    MOV x0, 1
+    LDR x1, =\texto
+    LDR x2, =\cantidad
+    MOV x8, 64        
+    SVC 0 
+.endm
+
+//metodo input para obtener los valores ingresados en la consola
+.macro input
+    MOV x0, 0
+    LDR x1, =opcion
+    LDR x2, =5 //es el tamaño bss .space 5 rservado para la variable de entrada
+    MOV x8, 63
+    SVC 0
+.endm
+
+
+.text
+_start:
+    print clear, lenClear //limpia la terminal
+    print encabezado, lenEncabezado //muestra mensaje encabezado en la terminal
+    input
+
+menu:
+    print clear, lenClear //limpia la terminal
+    print menuPrincipal, lenMenu //muestra mensaje menuPrincipal en la terminal
+    print Opcion, lenOpcion //muestra mensaje Ingrese una opcion en la terminal
+    input 
+
+    LDR x10, =opcion //almacena la opcion ingresada en el registro x10
+    LDRB w10, [x10]
+
+    CMP w10, 49   // 49 es en codigo ascii equivale a 1
+    BEQ ingresoLista
+
+    CMP w10, 50
+    BEQ Bubble
+
+    CMP w10, 51
+    BEQ Quick
+
+    CMP w10, 52
+    BEQ Insertion
+
+    CMP w10, 53
+    BEQ Merge
+
+    CMP w10, 54  // 54 es en codigo ascii que equivale a 6, termina el programa
+    BEQ end
+
+    ingresoLista:
+        print clear, lenClear //limpia la terminal
+        print subMenu, lensubMenu //muestra mensaje subMenu en la terminal
+        print Opcion, lenOpcion //muestra mensaje Ingrese una opcion en la terminal
+        input 
+
+        LDR x10, =opcion //almacena la opcion ingresada en el registro x10
+        LDRB w10, [x10]
+
+        CMP w10, 49   // 49 es en codigo ascii equivale a 1
+        BEQ manual
+
+        CMP w10, 50
+        BEQ archivoCSV
+
+        CMP w10, 51
+        BEQ regresar
+
+        B cont // B = branch incondicional - se va a cont
+    
+    cont:
+        input
+        B menu // B = branch incondicional - se va a menu, lo cual simula un ciclo while
