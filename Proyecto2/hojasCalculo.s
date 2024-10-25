12feb5edee78d2229a0195686941568da11a9342
LuisReal <lgonzalezr2611@gmail.com>
2024-10-24 22:11:54 -0600

    verifyCommand: llenar, importar
    codigo en caso venga el comando llenar e importar

--- Proyecto2/hojasCalculo.s
+++ Proyecto2/hojasCalculo.s
@@ -293,8 +293,16 @@ _start:
         
         ldr x0, =comando            // Se carga la direccion de memoria del comando
         ldrb w20, [x0], 1           // Se carga el primer caracter en w20
+        
         cmp w20, #'G'               // Compara el carácter con 'G'
         beq guardar                 // Si es igual, salta a guardar
+        
+        cmp w20, #'L'
+        beq llenar                  // Si es igual, salta a llenar
+        
+        cmp w20, #'I'               // Compara el carácter con 'I'
+        beq importar                // Si es igual, salta a importar
+        
         b end_verify                // Si no encuentra ningun comando finaliza verificacion
         
         guardar:
@@ -327,6 +335,96 @@ _start:
             bne end_verify          // Si no es igual, salta a end_verificar
 
             mov w4, 1               // (Comando Guardar encontrado)
+            B end_verify     // Si no encuentra ningun comando marcar error
+
+        llenar:
+            ldrb w20, [x0], #1  // Se sigue avanzando en el buffer (comando)
+            cmp w20, #'L'          // Compara el carácter con 'U'
+            bne end_verify           // Si no es igual, salta a end_verify
+
+            ldrb w20, [x0], #1  // Se sigue avanzando en el buffer (comando)
+            cmp w20, #'E'          // Compara el carácter con 'U'
+            bne end_verify           // Si no es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'N'          // Compara el carácter con 'A'
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'A'          // Compara el carácter con 'A'
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'R'          // Compara el carácter con 'R'
+            bne end_verify           // Si es igual, salta a end_verify
+            
+            ldrb w20, [x0], #1
+            cmp w20, #' '          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'D'          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'E'          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'S'          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'D'          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'E'          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #' '          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            mov w4, 11   // w4=11 (Comando Llenar encontrado)
+            b end_verify     // Si no encuentra ningun comando marcar error
+            
+        importar:
+            ldrb w20, [x0], #1  // Se sigue avanzando en el buffer (comando)
+            cmp w20, #'M'          // Compara el carácter con 'U'
+            bne end_verify           // Si no es igual, salta a end_verify
+
+            ldrb w20, [x0], #1  // Se sigue avanzando en el buffer (comando)
+            cmp w20, #'P'          // Compara el carácter con 'U'
+            bne end_verify           // Si no es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'O'          // Compara el carácter con 'A'
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'R'          // Compara el carácter con 'A'
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'T'          // Compara el carácter con 'R'
+            bne end_verify           // Si es igual, salta a end_verify
+            
+            ldrb w20, [x0], #1
+            cmp w20, #'A'          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #'R'          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            ldrb w20, [x0], #1
+            cmp w20, #' '          // Compara el carácter con ' '
+            bne end_verify           // Si es igual, salta a end_verify
+
+            MOV w4, 15   // w4=15 (Comando Importar encontrado)
+            B end_verify     // Si no encuentra ningun comando marcar error
+    
 
         end_verify:
             ret
@@ -486,100 +584,100 @@ _start:
         cmp w20, #'S'          // Compara el carácter con 'S'
         beq separado_por       
         
-        b fin_verificar
+        b end_verify
         
         palabra_en:
         
             ldrb w20, [x0], #1
             cmp w20, #'N'        
-            bne fin_verificar    
+            bne end_verify    
 
             ldrb w20, [x0], #1
             cmp w20, #' '        
-            bne fin_verificar    
+            bne end_verify    
 
             mov w4, 1           // w4=1 palabra intermedia EN encontrada
-            b fin_verificar
+            b end_verify
 
         palabra_hasta:
 
             ldrb w20, [x0], #1
             cmp w20, #'A'          
-            bne fin_verificar            
+            bne end_verify            
 
             ldrb w20, [x0], #1
             cmp w20, #'S'          
-            bne fin_verificar            
+            bne end_verify            
 
             ldrb w20, [x0], #1
             cmp w20, #'T'          
-            bne fin_verificar            
+            bne end_verify            
 
             ldrb w20, [x0], #1
             cmp w20, #'A'          
-            bne fin_verificar            
+            bne end_verify            
 
             ldrb w20, [x0], #1
             cmp w20, #' '          
-            bne fin_verificar            
+            bne end_verify            
 
             mov w4, 2                   // w4=2 palabra intermedia HASTA encontrada
-            b fin_verificar
+            b end_verify
 
         separado_por:
 
             ldrb w20, [x0], #1
             cmp w20, #'E'           
-            bne fin_verificar       
+            bne end_verify       
 
             ldrb w20, [x0], #1
             cmp w20, #'P'           
-            bne fin_verificar       
+            bne end_verify       
 
             ldrb w20, [x0], #1
             cmp w20, #'A'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #'R'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #'A'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #'D'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #'O'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #' '            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #'P'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #'O'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #'R'            
-            bne fin_verificar        
+            bne end_verify        
 
             ldrb w20, [x0], #1
             cmp w20, #' '            
-            bne fin_verificar        
+            bne end_verify        
 
             mov w4, 3               // w4=3 palabra intermedia SEPARADO POR encontrada
-            b fin_verificar
+            b end_verify
 
-        fin_verificar:
+        end_verify:
             ret
     
     posicionACelda:
